21d34d874aacf0d5310d36d86278bfb3cf8d98d2
HarryCoburn <harryjcoburn@gmail.com>
2026-06-02 11:09:10 -0400

    Command registration and running test command.

--- commands.go
+++ commands.go
@@ -1,6 +1,39 @@
 package main
 
+import "fmt"
+
 type command struct {
 	name string
 	args []string
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	elem, ok := c.commands[cmd.name]
+	if !ok {
+		return fmt.Errorf("Command %s is not registered", cmd.name)
+	}
+	err := elem(s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.commands[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if cmd.args == nil {
+		return fmt.Errorf("Login command requires a username.")
+	}
+	s.state.SetUser(cmd.args[0])
+
+	fmt.Printf("Username set to %s\n", cmd.args[0])
+	return nil
+
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/HarryCoburn/blog-aggregator/internal/config"
 )
@@ -11,17 +12,42 @@ type state struct {
 }
 
 func main() {
+	var appState state
+	var commands commands
+
+	// Initialize state
 	file, err := config.Read()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	file.SetUser("harry")
-	file, err = config.Read()
-	if err != nil {
-		fmt.Println(err)
-		return
+	appState.state = &file
+
+	// Initialize command map
+	commands.commands = make(map[string]func(*state, command) error)
+
+	// Register commands
+	commands.register("login", handlerLogin)
+
+	// Check arguments
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Not enough arguments provided. Exiting.")
+		os.Exit(1)
 	}
-	fmt.Printf("%+v\n", file)
+
+	// Build command
+	var command command
+	command.name = args[1]
+	if len(args) > 2 {
+		command.args = args[2:]
+	}
+
+	// Run command
+	err = commands.run(&appState, command)
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+		os.Exit(1)
+	}
 
 }
